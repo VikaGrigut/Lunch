495d4c3c6564b0970618c3855703c870196e9161
Vika Grigut <grigutvika@gmail.com>
2024-06-06 00:42:06 +0300

    Initial commit

---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class MainKtTest {
+
+    @Test
+    fun differenceOfBills1() {
+        val bill = intArrayOf(2, 4, 6)
+        val k = bill[2]
+        val b = 6
+        val expected = 3
+        val result = differenceOfBills(bill, k, b)
+        assertEquals(expected,result)
+    }
+
+    @Test
+    fun differenceOfBills2() {
+        val bill = intArrayOf(5,8,6,7)
+        val k = bill[1]
+        val b = 13
+        val expected = 4
+        val result = differenceOfBills(bill, k, b)
+        assertEquals(expected,result)
+    }
+
+    @Test
+    fun differenceOfBills3() {
+        val bill = intArrayOf(10,5,16,3)
+        val k = bill[2]
+        val b = 9
+        val expected = 0
+        val result = differenceOfBills(bill, k, b)
+        assertEquals(expected,result)
+    }
+}
